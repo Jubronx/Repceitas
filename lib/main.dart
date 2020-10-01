1b00cfc01d4132e75ff206162fdb1656efdd33b2
flavioyamada <45017643+flavioyamada@users.noreply.github.com>
2020-10-01 00:56:32 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:repceitas_app/detailsPage.dart';
+import 'package:repceitas_app/telaPrincipal.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,142 +8,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
+        home: WidgetLogin(),
+        debugShowCheckedModeBanner: false,
+        title: "Login",
+        theme: ThemeData(primaryColor: Colors.white));
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class WidgetLogin extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _WidgetLoginState createState() => _WidgetLoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _WidgetLoginState extends State<WidgetLogin> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   @override
   Widget build(BuildContext context) {
+    final emailField = TextField(
+        obscureText: false,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Email",
+            hintStyle: TextStyle(color: Colors.white),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(32.0),
+                borderSide: BorderSide(color: Colors.white))));
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Senha",
+          hintStyle: TextStyle(color: Colors.white),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final buttonLogin = ButtonTheme(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        child: RaisedButton(
+            color: Color(0xFFffffff),
+            child: Text("Login",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Color(0xFF7d0633), fontWeight: FontWeight.bold)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+              );
+            },
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0))));
+
     return Scaffold(
-      backgroundColor: Color(0xFF7d0633),
-      body: ListView(
+        body: Center(
+            child: Container(
+      color: Color(0xFF7d0633),
+      padding: const EdgeInsets.all(40.0),
+      child: Column(
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 15.0, left: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  color: Colors.white,
-                  onPressed: () {},
-                ),
-                Container(
-                    width: 125.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.filter_list),
-                          color: Colors.white,
-                          onPressed: () {},
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.menu),
-                          color: Colors.white,
-                          onPressed: () {},
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          ),
-          SizedBox(height: 30.0),
-          Padding(
-            padding: EdgeInsets.only(left: 40.0),
-            child: Row(
-              children: <Widget>[
-                Text('Repceitas',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 35.0)),
-                SizedBox(width: 10.0),
-                Text('para republicas',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        color: Colors.white,
-                        fontSize: 18.0))
-              ],
-            ),
+          SizedBox(
+            height: 150.0,
           ),
           SizedBox(height: 40.0),
-          Container(
-            height: MediaQuery.of(context).size.height - 185.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-            ),
-            child: ListView(
-              primary: false,
-              padding: EdgeInsets.only(left: 25.0, right: 20.0),
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(top: 45.0),
-                    child: Container(
-                        height: MediaQuery.of(context).size.height - 300.0,
-                        child: ListView(children: [
-                          _buildFoodItem(
-                              'assets/plate1.png', 'Macarrão', 'lorem ipsum'),
-                          _buildFoodItem(
-                              'assets/plate2.png', 'Feijão', 'lorem ipsum'),
-                          _buildFoodItem(
-                              'assets/plate6.png', 'Arroz', 'lorem ipsum'),
-                          _buildFoodItem(
-                              'assets/plate5.png', 'Salada', 'lorem, ipsum')
-                        ]))),
-              ],
-            ),
-          )
+          emailField,
+          SizedBox(height: 20.0),
+          passwordField,
+          SizedBox(height: 40.0),
+          buttonLogin,
         ],
       ),
-    );
-  }
-
-  Widget _buildFoodItem(String imgPath, String foodName, String receita) {
-    return Padding(
-        padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
-        child: InkWell(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => DetailsPage(
-                      heroTag: imgPath,
-                      foodName: foodName,
-                      foodReceita: receita)));
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    child: Row(children: [
-                  Hero(
-                      tag: imgPath,
-                      child: Image(
-                          image: AssetImage(imgPath),
-                          fit: BoxFit.cover,
-                          height: 75.0,
-                          width: 75.0)),
-                  SizedBox(width: 10.0),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(foodName,
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 17.0,
-                                fontWeight: FontWeight.bold)),
-                      ])
-                ])),
-              ],
-            )));
+    )));
   }
 }
